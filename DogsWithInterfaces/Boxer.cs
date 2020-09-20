6892eb40f4fcc2097d20f7574fee37437c691fa6
comptosk@gmail.com <comptosk@gmail.com>
2020-09-20 17:34:39 -0400

    Add project files.

---
+++ DogsWithInterfaces/Boxer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogsWithInterfaces {
+    class Boxer : IDog {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string BarkSound { get; set; }
+
+        public void Bark() {
+            Console.WriteLine($"A Boxer goes {BarkSound}");
+        }
+    }
+}
---
+++ DogsWithInterfaces/IDog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogsWithInterfaces {
+    interface IDog {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string BarkSound { get; set; }
+
+        public void Bark();
+
+    }
+}
+    
+
